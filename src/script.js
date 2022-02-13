83663efac50511a998d366a374142f35e98f0f35
jmalderle <96013438+jmalderle@users.noreply.github.com>
2022-02-13 18:53:08 +0100

    added functions to show current temp for searched city weather

--- src/script.js
+++ src/script.js
@@ -30,6 +30,26 @@ let month = months[now.getMonth()];
 let todaysDate = document.querySelector(".current-date");
 todaysDate.innerHTML = `It’s ${day},  ${now.getDate()}. ${month} of ${now.getFullYear()}`;
 
+function showTemp(response) {
+  console.log();
+  let tempForSearchInput = document.querySelector(".current-temperature");
+  tempForSearchInput.innerHTML = `${Math.round(response.data.main.temp)}°`;
+}
+
+function locateCityWeather(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input"); // search form input
+  let newHeading = document.querySelector(".current-city");
+  newHeading.innerHTML = `${searchInput.value}`;
+  let units = "metric";
+  let apiKey = "ceb455b9c7b642371d7b3ed2d734c15b";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiEndpoint}?q=${searchInput.value}&appid=${apiKey}&units=${units}`;
+  let tempForCity = console.log(apiUrl);
+
+  axios.get(apiUrl).then(showTemp);
+}
+
 let searchForm = document.querySelector(".search-form");
 
 searchForm.addEventListener("submit", locateCityWeather);
