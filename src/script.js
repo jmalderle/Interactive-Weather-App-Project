28a8ea41a97b04b007da9cb474ce329c157d18b3
jmalderle <96013438+jmalderle@users.noreply.github.com>
2022-02-20 22:10:53 +0100

    made some changes bc I wanted to add a gif, at this point code isnt working yet

--- src/script.js
+++ src/script.js
@@ -47,7 +47,6 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
-  console.log(response);
 }
 
 function search(city) {
